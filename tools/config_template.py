0fe853d18c6dceb06acfbec864f17478f83eff46
Alessandro <31077802+alezm00@users.noreply.github.com>
2023-11-18 22:05:38 +0100

    tools update

--- tools/config_template.py
+++ tools/config_template.py
@@ -1,33 +1,15 @@
-
-
-#SETTINGS
-serverPath = "C:\Program Files (x86)\Steam\steamapps\common\Arma 3 Server"
-serverDevPath = f"{serverPath}\GENDEV"
-
-
-
-MOD_LIST = [
-    "E:\SteamLibrary\steamapps\common\Arma 3\!Workshop\@CBA_A3",
-    "E:\SteamLibrary\steamapps\common\Arma 3\!Workshop\@ace",
-    "E:\SteamLibrary\steamapps\common\Arma 3\!Workshop\@Task Force Arrowhead Radio (BETA!!!)",
-    "F:\GITS\9GU\DataMod\Releases\Latest\@9GUGameData",
-]
-
-modName = "@9GUGameData"
+modName = "@AZM_BFT"
 BUILD_LIST = [
-    'gen_data',
-    'gen_markers'
+    'azm_bft'
 ]
 
 addonBuilder = "C:\Program Files (x86)\Steam\steamapps\common\Arma 3 Tools\AddonBuilder\AddonBuilder.exe"
 addonBuilder_IncludesPath = 'tools\\addonBuilder_Includes.txt'
 
-
 #mods that dont need to be built but only signed
-SIGN_LIST = [
-    'afm_acre.pbo'
-]
+SIGN_LIST = []
 
 DSSignFile = "C:\Program Files (x86)\Steam\steamapps\common\Arma 3 Tools\DSSignFile\DSSignFile.exe"
 
-privateKEY = "F:\GITS\9GU\SignKey\9GU.biprivatekey"
+
+privateKEY = "F:\GITS\Blue-Force-Tracking\SignKey\AZMBFT.biprivatekey"
